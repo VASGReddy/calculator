2b2955eb2b13f3f1089697936c2f4565476e2d63
Ganeswara Reddy <84664852+VASGReddy@users.noreply.github.com>
2021-06-07 22:05:04 +0530

    used JS interface to convert text into speech
    Used SpeechSynthesisUtterance() which converts text into speech in javascript

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ function print_btnvalue(btnvalue)
 //function that evaluates selected buttons(both operands and operators) and returns result
 function calculator_result()
 {
+    if(document.getElementById("resultscreen").value=='')
+    document.getElementById("resultscreen").value='';
+    else{
     let x = document.getElementById("resultscreen").value;
-    x = x.replace(/÷/g, "/"); // Replacing '÷' symbol with '/'
-    x = x.replace(/x/g,"*"); // Replacing 'x' symbol wth '*'
+    x = x.replace(/÷/g, "/"); // Replacing '÷' symbol with '/' using Regex
+    x = x.replace(/x/g,"*"); // Replacing 'x' symbol wth '*' using Regex
     let y = eval(x); // JS functin which evaluates given string and returns its value
     document.getElementById("resultscreen").value=y;
+    }
 }
 
 function reset_btn()
@@ -21,5 +25,15 @@ function reset_btn()
 
 function backspace_btn() {
     let value = document.getElementById("resultscreen").value;
-    document.getElementById("resultscreen").value = value.substr(0, value.length - 1);
+    document.getElementById("resultscreen").value = value.substr(0,value.length-1);
+}
+function hear_result() {
+    let msg = new SpeechSynthesisUtterance(); // Java script interface which creates its below objects
+    speech.lang = "en-US";
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    msg.text = document.getElementById("resultscreen").value;
+    window.speechSynthesis.speak(msg);
 }
